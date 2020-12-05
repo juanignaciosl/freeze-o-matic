cab5063944de661316408be49be11a5f42635d4b
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2020-12-05 20:17:52 +0100

    Upload tgz'ed directories and force uploading

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 A backup tool for files that you expect to never have to read again.
 
-This is essentially a personal project for backing up my personal files into a S3 bucket with Glacier Deep Archive, so
-don't expect anything too sophisticated ;)
+This is essentially a personal project for backing up my own files into a S3 bucket with Glacier Deep Archive, so don't
+expect anything too sophisticated ;) Use this at your own risk.
 
 ## Install
 
@@ -17,21 +17,36 @@ don't expect anything too sophisticated ;)
 2. The storage settings are at object level, not at bucket level, so create a folder, and then select it and set Glacier
    Deep Archive as storage class at Actions -> Select storage class.
 
-## Selecting what to backup
+## Selecting what to back up
 
 Copy `freezer.example.csv` to a known location and add there one line per path that you want to upload. Each file has
-two elements, separated by commas:
+the following elements, separated by commas:
 
 - Source file (if it's a relative path, it must be relative to the path that you run it from).
 - Target path in the S3.
+- Storage class. One of the following: STANDARD, REDUCED_REDUNDANCY, STANDARD_IA, ONEZONE_IA, INTELLIGENT_TIERING,
+  GLACIER, DEEP_ARCHIVE, OUTPOSTS. Defaults to DEEP_ARCHIVE.
+- force/nothing: whether the file should be always uploaded, no matter the status at the lock file. Useful for the
+  freezer files, for example.
 
 Example:
 
-    freezer.example.csv,freezer.example.csv
+    freezer.example.csv,freezer.example.csv,STANDARD,force
 
-That declares that you want to backup the freezer.example.csv file to the root of the bucket, with the same name. You're
-highly encourage to backup the freezer and lock (see Running section) files as well. Add them at the end of the file so
-they're copied after all other files are.
+That declares that you want to back up the freezer.example.csv file to the root of the bucket, with the same name.
+You're highly encouraged to back up the freezer and lock (see Running section) files as well (in STANDARD mode). Add
+them at the end of the file, so they're copied after all other files are.
+
+### Directory backup
+
+If you want to back up a full directory instead of a file, write the directory as the source and as target, pick a path
+ending in `.tar.gz`. The directory will be packed, compressed and uploaded to the target destination.
+
+Example:
+
+    /tmp/test,my-glacier-dir/test.tar.gz,
+
+_Please note the trailing comma, meaning that the default storage class (Deep Archive) will be used_.
 
 ## Running
 
@@ -40,5 +55,15 @@ Freeze-o-matic uses boto3 for S3 uploading, so check
 
     AWS_PROFILE=myprofile python -m freezeomatic.run --bucket mybucket --freezer freezer.example.csv
 
-Running it creates a `<freezer>.lock` file in the same path than the freezer file. That file reflects the status of the
+Running it creates a `<freezer>.lock` file in the same path as the freezer file. That file reflects the status of the
 upload process.
+
+## TODO
+
+Things that I might eventually do:
+
+- [x] Force upload already frozen files.
+- [ ] Force upload if storage class changes.
+- [ ] Support for the download lifecycle.
+- [ ] Check if files at the freezer should actually be uploaded or not (if the content is different). Right now, as a
+  file is marked as frozen at the lock file, it won't be uploaded.
--- freezeomatic/run.py
+++ freezeomatic/run.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 import csv
 import os
+import tarfile
 from argparse import ArgumentParser
 from dataclasses import dataclass, replace
 from enum import Enum
-from typing import List
+from tempfile import gettempdir
+from typing import List, Any
 
 import boto3
 
@@ -13,10 +15,26 @@ from freezeomatic.utils import get_logger
 logger = get_logger(__name__)
 
 
+class StorageClass(Enum):
+    STANDARD = 'STANDARD'
+    REDUCED_REDUNDANCY = 'REDUCED_REDUNDANCY'
+    STANDARD_IA = 'STANDARD_IA'
+    ONEZONE_IA = 'ONEZONE_IA'
+    INTELLIGENT_TIERING = 'INTELLIGENT_TIERING'
+    GLACIER = 'GLACIER'
+    DEEP_ARCHIVE = 'DEEP_ARCHIVE'
+    OUTPOSTS = 'OUTPOSTS'
+
+
+DEFAULT_STORAGE = StorageClass.DEEP_ARCHIVE
+
+
 @dataclass(frozen=True)
 class FreezerEntry:
     source_path: str
     target_path: str
+    storage_class: StorageClass
+    force: bool
 
 
 class LockStatus(Enum):
@@ -33,13 +51,15 @@ UPLOADED_STATUSES = {LockStatus.FROZEN, LockStatus.DEPRECATED}
 class FreezerLockEntry:
     target_path: str
     source_path: str
+    storage_class: StorageClass
     status: LockStatus
+    force: bool
 
     def deprecate(self) -> FreezerLockEntry:
         return replace(self, status=LockStatus.DEPRECATED)
 
-    def uploaded(self) -> bool:
-        return self.status in UPLOADED_STATUSES
+    def requires_upload(self) -> bool:
+        return self.status not in UPLOADED_STATUSES or self.force
 
 
 def freeze(bucket: str, freezer_path: str) -> int:
@@ -59,7 +79,13 @@ def _read_freezer(path: str) -> List[FreezerEntry]:
     with open(path, newline='') as freezer_file:
         reader = csv.reader(freezer_file, delimiter=',')
         for row in reader:
-            freezer.append(FreezerEntry(*row))
+            freezer.append(FreezerEntry(
+                source_path=row[0],
+                target_path=row[1],
+                storage_class=StorageClass(row[2])
+                if row[2] != '' else DEFAULT_STORAGE,
+                force=row[3] == 'force'
+            ))
     return freezer
 
 
@@ -70,7 +96,13 @@ def _read_lock(lock_path: str) -> List[FreezerLockEntry]:
             reader = csv.reader(lock_file, delimiter=',')
             for row in reader:
                 lock.append(
-                    FreezerLockEntry(row[0], row[1], LockStatus(row[2])))
+                    FreezerLockEntry(
+                        target_path=row[0],
+                        source_path=row[1],
+                        storage_class=StorageClass(row[2]),
+                        status=LockStatus(row[3]),
+                        force=row[4] == 'force'
+                    ))
     return lock
 
 
@@ -85,7 +117,9 @@ def _update_lock_entries(freezer_entries: List[FreezerEntry],
             lock_entry = FreezerLockEntry(
                 freezer_entry.target_path,
                 freezer_entry.source_path,
-                LockStatus.PENDING)
+                freezer_entry.storage_class,
+                LockStatus.PENDING,
+                freezer_entry.force)
         updated.append(lock_entry)
 
     for lock_entry in lock_entries:
@@ -101,15 +135,40 @@ def _upload(lock_path: str, lock_entries: List[FreezerLockEntry],
             bucket: str) -> None:
     s3 = boto3.client('s3')
     _dump_lock(lock_path, lock_entries)
-    pending = [e for e in lock_entries if not e.uploaded()]
+    pending = [e for e in lock_entries if e.requires_upload()]
     for entry in pending:
         entry.status = LockStatus.FREEZING
         _dump_lock(lock_path, lock_entries)
-        logger.debug(f'Uploading {entry}...')
-        s3.upload_file(entry.source_path, bucket, entry.target_path,
-                       ExtraArgs={'ServerSideEncryption': 'AES256'})
+        _upload_entry(s3, entry, bucket)
         entry.status = LockStatus.FROZEN
         _dump_lock(lock_path, lock_entries)
+
+
+def _upload_entry(s3: Any, entry: FreezerLockEntry, bucket: str) -> None:
+    source_path = entry.source_path
+    target_path = entry.target_path
+    storage_class = entry.storage_class
+    if os.path.isfile(source_path):
+        _upload_file(s3, source_path, bucket, target_path, storage_class)
+    elif os.path.isdir(source_path) and target_path.endswith('tar.gz'):
+        local_tgz = os.path.join(gettempdir(),
+                                 f'{os.path.basename(source_path)}.tar.gz')
+        logger.debug(f'Compressing {source_path} into {local_tgz}...')
+        with tarfile.open(local_tgz, 'w|gz') as tar:
+            tar.add(source_path, arcname=os.path.basename(source_path))
+
+        _upload_file(s3, local_tgz, bucket, target_path, storage_class)
+        os.remove(local_tgz)
+    else:
+        raise ValueError(f'Unsupported entry upload: {entry}')
+
+
+def _upload_file(s3: Any, source_path: str, bucket: str,
+                 target_path: str, storage_class: StorageClass) -> None:
+    logger.debug(f'Uploading {source_path} to {target_path}...')
+    s3.upload_file(source_path, bucket, target_path,
+                   ExtraArgs={'ServerSideEncryption': 'AES256',
+                              'StorageClass': storage_class.name})
 
 
 def _dump_lock(path: str, entries: List[FreezerLockEntry]) -> None:
@@ -117,7 +176,9 @@ def _dump_lock(path: str, entries: List[FreezerLockEntry]) -> None:
         writer = csv.writer(lock_file, delimiter=',')
         for entry in entries:
             writer.writerow(
-                [entry.target_path, entry.source_path, entry.status.value])
+                [entry.target_path, entry.source_path,
+                 entry.storage_class.name, entry.status.value,
+                 'force' if entry.force else ''])
 
 
 def main() -> int:
